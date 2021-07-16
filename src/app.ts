a381737db668ca0bdc3c591727f9491095a5ae85
Aiesha Brown <Aiesha.Brown@clorox.com>
2021-07-16 14:58:42 -0500

    starter template completed

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'
+
+require('dotenv').config();
+
+const app: Express = express()
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cloroxory.dei81.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+
+mongoose
+  .connect(uri, options)
+ const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.warn('Database Connected!');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
