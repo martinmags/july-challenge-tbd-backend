e2645c209eb1c8782bec0d29a1f4d273f98a89b7
Alexa Lewis <alexal@alexalebp132020.home>
2021-07-28 15:32:32 -0400

    database seeding started, have to add actual data we want to use

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,26 @@
-import express, { Express } from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors'
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import connect from "./scripts/dummy_data";
 
-require('dotenv').config();
+require("dotenv").config();
 
-const app: Express = express()
-const PORT: string | number = process.env.PORT || 4000
+const app: Express = express();
+const PORT: string | number = process.env.PORT || 4000;
 
-app.use(cors())
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cloroxory.dei81.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
+app.use(cors());
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cloroxory.dei81.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
-mongoose
-  .connect(uri, options)
- const db = mongoose.connection;
+mongoose.connect(uri, options);
+const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.warn('Database Connected!');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  connect;
+  console.warn("Database Connected!");
 });
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
---
+++ src/scripts/dummy_data.ts
@@ -0,0 +1,27 @@
+import { UserModel } from "../models/user_schema";
+
+const connect = async () => {
+  const users = [
+    { firstName: "Emma", lastName: "Bradley", age: 34 },
+    { firstName: "Elise", lastName: "Conner", age: 62 },
+    { firstName: "Jack", lastName: "Lawson", age: 20 },
+    { firstName: "Oliver", lastName: "Moss", age: 80 },
+    { firstName: "Jamie", lastName: "Reid", age: 52 },
+    { firstName: "Aidan", lastName: "Bradley", age: 73 },
+    { firstName: "Jordan", lastName: "Gallagher", age: 27 },
+    { firstName: "Erin", lastName: "Miles", age: 23 },
+    { firstName: "William", lastName: "May", age: 39 },
+    { firstName: "Ethan", lastName: "Butler", age: 68 },
+  ];
+
+  try {
+    for (const user of users) {
+      await UserModel.create(user);
+      console.log(`Created user ${user.firstName} ${user.lastName}`);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default connect;
